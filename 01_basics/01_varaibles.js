2db79549eb64f0c996138d7811d97c5acc9b0dec
Mandeep234567 <mandeeppathania222@gmail.com>
2024-08-15 15:47:07 +0000

    container config  code

---
+++ 01_basics/01_varaibles.js
@@ -0,0 +1,12 @@
+const accountId = 1445278
+let accountEmail = "mandeep@gmail.com"
+var accountPassword = "12345"
+accountCity ="pathankot"
+let accountState;
+// accountId = 2 // not allowed
+
+accountEmail = "navi@gmai;l.com"
+accountPassword ="3456778"
+accountCity = "gurdaspur"
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity, accountState])
---
+++ 01_basics/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are notejs not browers
+
+console.log(3+3) 
+console.log("Mandeep") // code readability should be high
+ let name = " mandeep"
+ let age = 18
+ let isLoggedId = false
+
+ // number is => 2 to power 53
+ // bigint
+
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined => 
+// symbol => unique
+
+
+
+ // object \
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); // undefined
