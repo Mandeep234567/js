a5daf3cba14353be480f3fc249b563fff892f428
Mandeep234567 <mandeeppathania222@gmail.com>
2024-08-16 17:13:29 +0000

    conversion to string

---
+++ 01_basics/stringToNumber.js
@@ -0,0 +1,56 @@
+ let value = 3
+ let negValue = -value
+ // console.log(negValue);
+ 
+
+ /*console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**3);
+ console.log(2/3);
+ console.log(2%3);
+ 
+ 
+ let str1 = "mandeep"
+ let str2 = " hello"
+
+ let str3 = str1 + str2
+console.log(str3);
+ 
+ console.log("1"+2);
+ console.log(1+"2");
+ console.log("1"+2+2);
+ console.log(1+2+"2");
+ 
+ console.log(3+4*5%6); // not to write code like this
+ 
+console.log(true);
+*/
+
+ let num1, num2, num3
+
+
+ num1 = num2 = num3 = 2+2
+
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
