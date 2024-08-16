cf296e9117b05942bb477d9af101434204ee307c
Mandeep234567 <mandeeppathania222@gmail.com>
2024-08-16 16:27:21 +0000

    conversion of datatype

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,35 @@
+/*let score = 
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0;
+// null => 0;
+// undefined => NaN
+// "abd" => NaN
+// 
+
+let isLoggedIn = "mandeep"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+    console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0 => false;
+// "" => false
+// "mandeep" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
