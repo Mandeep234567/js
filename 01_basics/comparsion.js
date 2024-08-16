13df5c0d43146ea81bb87ab876becc38faee7b05
Mandeep234567 <mandeeppathania222@gmail.com>
2024-08-16 17:28:04 +0000

    conversion of comparions

---
+++ 01_basics/comparsion.js
@@ -0,0 +1,25 @@
+/*console.log(2<1);
+console.log(2>1);
+console.log(2==1);
+console.log(2<=1);
+console.log(2!=1);
+
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0); // the reason is that an equality check == and comparsion <><>= work differnty. comparsion convert null to a number it as 0' gthats why (3) null >= 0 is true and (1) null >0 is false
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+*/
+
+
+// === 
+
+console.log("2" === 2);
+
+
