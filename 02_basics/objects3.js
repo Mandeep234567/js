0ff8b0758ab053032ccebeb07de7d4440584ced1
Mandeep234567 <mandeeppathania222@gmail.com>
2024-08-25 10:07:40 +0000

    objects part 3

---
+++ 02_basics/objects3.js
@@ -0,0 +1,30 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+//  course.courseInstructor
+
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+/*const navbar = ({company}){        objevts deconstruction
+}
+navbar(company = "mandeep")*/
+
+// json
+/*
+    "name": "mandeep",
+    "coursename": " js in hindi",
+    "price": "free"
+}
+    */
+  [
+    {
+    },
+    {
+    }
+  ] 
