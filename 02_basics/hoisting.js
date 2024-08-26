ddc1ab4fc5e0f43e92f97b5ea5e11045cd542c5a
Mandeep234567 <mandeeppathania222@gmail.com>
2024-08-26 16:18:34 +0000

    hoisting done

---
+++ 02_basics/hoisting.js
@@ -0,0 +1,39 @@
+/* function one(){
+    const username = "mandeep"
+
+    function two() {
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website);
+    // two()
+}
+ one()*/
+
+
+if(true){
+    const username = "mandeep"
+    if(username === "mandeep"){
+        const website = "youtube"
+       // console.log(username + website);
+        
+    }
+//    console.log(website);
+    
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++++ interesting +++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1 
+
+}
+
+addtwo(5)
+const addtwo = function(num){
+    return num + 2
+}
